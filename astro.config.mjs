f7a83fd2315a46c6d60088fb71430f9a58c4a8a4
Edgar Rojas <ejrl64@hotmail.com>
2023-11-29 21:35:05 -0400

    Commit2911202302

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from 'astro/config';
+import NetlifyCMS from 'astro-netlify-cms';
 import netlify from '@astrojs/netlify/functions';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'master',
+        },
+        collections: [ ],
+      },
+    }),],
   output: "server",
   adapter: netlify(),
 
